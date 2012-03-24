096aa966351dc78686609a5e7fa47cf161e1e0b9
Nick Sandberg <nss118@gmail.com>
2012-03-23 19:54:36 -0700

    traits_out_of_ships

--- db/migrate/20120322234820_adding_all_traits_to_ships_db.rb
+++ db/migrate/20120322234820_adding_all_traits_to_ships_db.rb
@@ -1,5 +1,5 @@
 class AddingAllTraitsToShipsDb < ActiveRecord::Migration
-  def change
+  def up
     change_table :ships do |t|
       t.boolean :agile, :default => false
       t.integer :anti_drone, :default => 0
@@ -19,5 +19,25 @@ class AddingAllTraitsToShipsDb < ActiveRecord::Migration
       t.integer :tractor_beam, :default => 0
       t.integer :transporter, :default => 0
     end
+  end
+  
+  def down
+    remove_column :ships, :agile
+    remove_column :ships, :anti_drone
+    remove_column :ships, :armoured
+    remove_column :ships, :cloak
+    remove_column :ships, :command
+    remove_column :ships, :enhanced_bridge
+    remove_column :ships, :fast
+    remove_column :ships, :immobile
+    remove_column :ships, :labs
+    remove_column :ships, :lumbering
+    remove_column :ships, :probes
+    remove_column :ships, :quick_launch
+    remove_column :ships, :scout
+    remove_column :ships, :slow
+    remove_column :ships, :stealth
+    remove_column :ships, :tractor_beam
+    remove_column :ships, :transporter
   end
 end
