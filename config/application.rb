a1aa86bbdac1e58f5d5d69db7b19bfc1c23a7c2f
Nick Sandberg <nss118@gmail.com>
2013-06-29 21:54:23 -0700

    precompile

--- config/application.rb
+++ config/application.rb
@@ -56,5 +56,6 @@ module Actabuilder
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assests.initialize_on_precompile = false
   end
 end
